aedfb28cba0bbe16e0e5b211f4a6cb2063184b3c
vignesh <vignesh.r@lxisoft.com>
2018-04-05 15:32:09 +0530

    NW-55

--- forest/Forest.java
+++ forest/Forest.java
@@ -1,14 +1,16 @@
 package com.lxisoft.animalgame.forest;
 import com.lxisoft.animalgame.animal.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Forest
 {
 	
- private ArrayList<Animal>animals=new ArrayList<>();
+	private ArrayList<Animal>animals=new ArrayList<>();
 
 private  String name;
 
+Scanner scan=new Scanner(System.in);
 
 
 public void createAnimals()
@@ -47,7 +49,7 @@ public void inputForestDetails()
 	setName("MALAMPUZHA FOREST");
 	System.out.println("********"+getName()+"********");
 	
-	System.out.println("*****ANIMALS ARE*****");
+	System.out.println("*********ANIMALS ARE*********");
     //  for(int j = 0;j<4;j++)
 	//{
 		
@@ -57,6 +59,8 @@ public void inputForestDetails()
 	System.out.println(animals.get(3).getName());*/
 	//}
 	initialization();
+	 inputGameImplimentation();
+	  animalGenertaion();
 	
 }
 
@@ -84,7 +88,6 @@ public void initialization()
 		if(i==3)
 		{
 			animals.get(i).inputTigerDetails();
-			
 			animals.get(i).setTigerLevels();
 		}
 		
@@ -93,15 +96,59 @@ public void initialization()
 }
 	public void inputGameImplimentation()
 	{
-		System.out.println("ANIMALS ARE ENTERED INTO THE GAME :");
-		System.out.println("ANIMALS ARE REDY TO FIGTH :");
+		
+		
+		System.out.println("                                                   ");
+		System.out.println("********ANIMALS ARE ENTERED INTO THE GAME**********");
+		System.out.println("***********ANIMALS ARE REDY TO FIGTH****************");
+		System.out.println("                                                   ");
+		
+		 int i = (int)(Math.random()*animals.size());
+		if(animals.get(i) instanceof Tiger)
+		{
+			
+			
+		}
+		
 	}
 
-
-
-
-
-
-
-
+   public void animalGenertaion()
+   {
+	   int ch=0;
+	   do{
+		  
+	   String animal1;
+	   String animal2;
+	   Animal a1 = animalSelection();
+	   Animal a2 = animalSelection();
+	   animal1 = a1.getTypeOfAnimal();
+	   animal2 = a2.getTypeOfAnimal();
+	   if(a1!=a2){
+		   System.out.println(animal1+" vs "+animal2);
+		   
+			if(a1.getStrengthLevel()>a2.getStrengthLevel() && a1.getHungryLevel()>a2.getHungryLevel())
+			{
+				System.out.println(a1.getTypeOfAnimal()+"WIN");
+			}
+			else{
+				System.out.println(a2.getTypeOfAnimal()+"WIN");
+			}
+		}
+		else{
+				System.out.println("same animal....... try again....");
+				
+		}
+		System.out.println("do you want to play again 1.yes/0.no");
+		ch=scan.nextInt();
+	   }while(ch==1);
+	}
+   
+   
+   public Animal animalSelection()
+   {
+	    
+		int x=(int)(Math.random()*animals.size());
+		Animal selectAnimal=animals.get(x);
+	    return selectAnimal;
+   }
 }
